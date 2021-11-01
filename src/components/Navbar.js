e82470c19a3a33ae6aebc8048524e4b35359c20a
Erick Felipe <erick.felipe@cda.lan>
2021-11-01 10:31:50 -0300

    add buttons on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,16 +5,39 @@ function Navbar() {
     const [click, setClick] = useState(false)
 
     const handleClick = () => setClick(!click)
+    const closeMobileMenu= () => setClick(false)
+    
     return (
         <>
         <nav className="navbar">
             <div className="navbar-container">
                 <Link to="/" className="navbar-logo">
-                Lagoa Mall <i className="fas fa-shopping-cart"/>
+                Test<i className="fas fa-shopping-cart"/>
                 </Link>
                 <div className="menu-icon" onClick={handleClick}> 
                     <i className={click ? "fas fa-times" : "fas fa-bars"} />
                 </div>
+                <ul className={click ? "nav-menu active" : "nav-menu"}></ul>
+                <li className="nav-item">
+                    <Link to="/home" className="nav-links" onClick={closeMobileMenu}>
+                    Inicio
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/stores" className="nav-links" onClick={closeMobileMenu}>
+                    Lojas
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/services" className="nav-links" onClick={closeMobileMenu}>
+                    Serviços
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/sign-up" className="nav-links" onClick={closeMobileMenu}>
+                    Cadastre-se
+                    </Link>
+                </li>
             </div>
         </nav>
         </>     
