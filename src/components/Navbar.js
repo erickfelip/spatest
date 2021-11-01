5d4a402e27fa6e157c8b52d394bc86ce9a047699
Erick Felipe <erick.felipe@cda.lan>
2021-11-01 10:19:11 -0300

    add event on menu icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,9 @@ import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
-    const [click, setClick] = 
+    const [click, setClick] = useState(false)
+
+    const handleClick = () => setClick(!click)
     return (
         <>
         <nav className="navbar">
@@ -10,7 +12,7 @@ function Navbar() {
                 <Link to="/" className="navbar-logo">
                 Lagoa Mall <i className="fas fa-shopping-cart"/>
                 </Link>
-                <div className="menu-icon"> 
+                <div className="menu-icon" onClick={handleClick}> 
                     <i className={click ? "fas fa-times" : "fas fa-bars"} />
                 </div>
             </div>
